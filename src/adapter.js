73193b4bd89b89329287f1a8e0c78ccf7a1a755b
github username <github email address>
2018-08-17 14:06:15 -0400

    Done.

--- src/adapter.js
+++ src/adapter.js
@@ -1,3 +1,21 @@
 class Adapter {
-  
+
+  static getDogList(){
+    return fetch(DOGGY_URL)
+      .then(res => res.json())
+  }
+
+  static getOneDog(dogId){
+    return fetch(`${DOGGY_URL}/${dogId}`)
+      .then(res => res.json())
+  }
+
+  static editDoggy(dogObj){
+    return fetch(`${DOGGY_URL}/${dogObj.id}`, {
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(dogObj),
+    })
+  }
+
 }
